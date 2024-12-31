45c298ff73a0865033fec4d18f448ce4085bf72f
IT22074072 <it22074072@my.sliit.lk>
2024-12-31 15:19:32 +0530

    schema and data file

--- src/main/java/org/example/springjdbc/SpringJdbcApplication.java
+++ src/main/java/org/example/springjdbc/SpringJdbcApplication.java
@@ -14,7 +14,7 @@ public class SpringJdbcApplication {
     public static void main(String[] args) {
         ApplicationContext context = SpringApplication.run(SpringJdbcApplication.class, args);
         Student s = context.getBean(Student.class);
-        s.setRollNo(101);
+        s.setRollNo(104);
         s.setName("Dinithi");
         s.setMarks(98);
 
---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+insert into student (rollno, name, marks) values (101, 'Jin', 78);
+insert into student (rollno, name, marks) values (102, 'Jimin', 78);
+insert into student (rollno, name, marks) values (103, 'Rm', 78);
