ee57ac3ad7d9b80995a87e2797b0d135275aff62
IT22074072 <it22074072@my.sliit.lk>
2024-12-31 09:55:04 +0530

    Student model

--- src/main/java/org/example/springjdbc/SpringJdbcApplication.java
+++ src/main/java/org/example/springjdbc/SpringJdbcApplication.java
@@ -1,13 +1,20 @@
 package org.example.springjdbc;
 
+import org.example.springjdbc.model.Student;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class SpringJdbcApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringJdbcApplication.class, args);
+        ApplicationContext context = SpringApplication.run(SpringJdbcApplication.class, args);
+        Student s = context.getBean(Student.class);
+        s.setRollNo(101);
+        s.setName("Dinithi");
+        s.setMarks(98);
+
     }
 
 }
